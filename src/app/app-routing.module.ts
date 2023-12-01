347a1d08155b56c827e85c9ac130757c3a55ba49
aledure <mandrew.ledure@gmail.com>
2023-12-01 07:48:03 -0600

    api data saved to local, imported to results

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { FilteredResultsComponent } from './components/filtered-results/filtered-results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'results', component: FilteredResultsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/filtered-results/filtered-results.component.ts
+++ src/app/components/filtered-results/filtered-results.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../shared/services/api.service';
+import { Lift } from '../../shared/models/lift.model';
 
 @Component({
   selector: 'app-filtered-results',
   templateUrl: './filtered-results.component.html',
-  styleUrls: ['./filtered-results.component.scss']
+  styleUrls: ['./filtered-results.component.scss'],
 })
 export class FilteredResultsComponent {
+  lifts: Lift[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.getLifts().subscribe((lifts) => {
+      this.lifts = lifts;
+    });
+  }
+
+  getLifts() {
+    return this.apiService.getAllLifts();
+  }
 }
