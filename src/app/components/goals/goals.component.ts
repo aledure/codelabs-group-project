4f1f86e24e82f93b9357a50a74083017e592b81b
Brooklyn Sides <brooklynjsides@gmail.com>
2023-12-14 18:00:50 -0600

    Merge Brooklyn into Master

--- src/app/components/goals/goals.component.ts
+++ src/app/components/goals/goals.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-goals',
   templateUrl: './goals.component.html',
-  styleUrls: ['./goals.component.scss']
+  styleUrls: ['./goals.component.scss'],
 })
-export class GoalsComponent {
-
-}
+export class GoalsComponent {}
